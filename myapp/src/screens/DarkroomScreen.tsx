c626389d156a5ab899b9867aeac2797cad44c4b2
poip116 <zpoipz.sezep@gmail.com>
2026-02-19 14:54:02 +0900

    fix

--- myapp/src/screens/DarkroomScreen.tsx
+++ myapp/src/screens/DarkroomScreen.tsx
@@ -4,7 +4,7 @@ import { Audio } from 'expo-av'; //expoのAudioをインポート
 import { getPhotosByStatus, updatePhotoStatus } from '../utils/sqlite';
 import * as MediaLibrary from 'expo-media-library';
 import { Modal } from 'react-native';
-import { StatusBar } from 'react-native';
+
 // 現像処理の画面
 interface DarkroomScreenProps {
   onBack: () => void;
